05a7cc8edb5213144cedbca8db6858361275b3aa
Artur Yakimovich, PhD <a.yakimovich@ucl.ac.uk>
2017-03-28 18:35:14 +0100

    updating tests

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+
+
+'''
+import pytest
+import math
+import cv2
+from scipy import ndimage
+from skimage import measure,morphology, io, filters
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+'''
+
+
+print('running tests...')
+
